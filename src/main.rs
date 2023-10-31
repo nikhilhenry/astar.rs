fe07b8210ea09ea380a4fdaf6a082476950439f2
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-10-31 17:07:29 +0530

    create base ui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use eframe::egui::Context;
+use eframe::{egui, Frame};
+
 fn main() {
-    println!("Hello, nikhil!");
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Path Finding",
+        options,
+        Box::new(|_cc| {
+            Box::new(MyApp {
+                title: "Hello from egui".to_string(),
+            })
+        }),
+    )
+    .expect("failed to initialise app")
+}
+
+struct MyApp {
+    title: String,
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading(&self.title);
+        });
+    }
 }
