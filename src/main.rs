cd4f962bf9fe54093b37c16d700449de8d89b097
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-11-10 09:59:12 +0530

    create a grid

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use eframe::egui::Context;
+use eframe::egui::{Context, Sense};
 use eframe::{egui, Frame};
 
 fn main() {
@@ -8,7 +8,8 @@ fn main() {
         options,
         Box::new(|_cc| {
             Box::new(MyApp {
-                title: "Hello from egui".to_string(),
+                height: 10,
+                width: 10,
             })
         }),
     )
@@ -16,13 +17,33 @@ fn main() {
 }
 
 struct MyApp {
-    title: String,
+    height: u32,
+    width: u32,
 }
 
 impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
+    fn update(&mut self, ctx: &Context, frame: &mut Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading(&self.title);
+            let window_size = frame.info().window_info.size;
+            let padding = egui::Vec2::new(10.0, 10.0);
+            // compute the rect_size
+            let rect_size = egui::Vec2::new(
+                (window_size.x - 20.0) / self.width as f32,
+                (window_size.y - 20.0) / self.height as f32,
+            );
+            let (_, painter) = ui.allocate_painter(window_size + padding, Sense::hover());
+            for y in 0..self.height {
+                for x in 0..self.width {
+                    let pos =
+                        egui::pos2(x as f32 * rect_size.x + 10.0, y as f32 * rect_size.y + 10.0);
+                    let rect = egui::Rect::from_min_size(pos, rect_size);
+                    painter.rect_stroke(
+                        rect,
+                        egui::Rounding::default(),
+                        egui::Stroke::new(1.0, egui::Color32::LIGHT_RED),
+                    );
+                }
+            }
         });
     }
 }
