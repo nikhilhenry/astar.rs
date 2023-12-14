22ced0b3d79606a57f2ff606cced081cfc3a0d94
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-12-14 23:46:27 +0530

    because class exists

--- src/position.rs
+++ src/position.rs
@@ -16,13 +16,13 @@ pub fn euclidean_distance(from: &Position, to: &Position) -> usize {
 pub fn manhattan_distance(from: &Position, to: &Position) -> usize {
     let x_dist = (from.x - to.x).abs();
     let y_dist = (from.y - to.y).abs();
-    (x_dist + y_dist) as usize
+    ((x_dist + y_dist) * 10) as usize
 }
 
 pub fn diagonal_distance(from: &Position, to: &Position) -> usize {
     let x_dist = (from.x - to.x).abs();
     let y_dist = (from.y - to.y).abs();
-    cmp::max(x_dist, y_dist) as usize
+    (cmp::max(x_dist, y_dist) * 10) as usize
 }
 
 impl Position {
